d9fc3b93f8a103024d9bb22028b2af86ed71a646
ksharathkrishna <sharathkrishna1234@gamil.com>
2019-08-17 23:03:14 +0530

    Reviewed

--- SCMS/src/webLayer/DateTets.java
+++ SCMS/src/webLayer/DateTets.java
@@ -5,11 +5,11 @@ import java.util.GregorianCalendar;
 
 public class DateTets {
     public static void main(String[] args) {
+
         Calendar cal = GregorianCalendar.getInstance();
-         //cal.get(Calendar.HOUR_OF_DAY);
-       // int hr = Calendar.HOUR_OF_DAY;
+
         int hr = cal.get(Calendar.HOUR_OF_DAY);
-        //int hr2 = Calendar.
+
         System.out.println(cal.get(Calendar.HOUR_OF_DAY) );
       System.out.println(hr);
     }
